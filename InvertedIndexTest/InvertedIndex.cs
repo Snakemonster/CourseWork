397f136d018e85ae523d906c517d2f39e2b4c5c5
Makuta <nikita.o.platonov@gmail.com>
2023-01-07 21:53:40 +0200

    Performance test

---
+++ InvertedIndexTest/InvertedIndex.cs
@@ -0,0 +1,79 @@
+using System.Collections.Concurrent;
+
+class InvertedIndex
+{
+    private ConcurrentDictionary<string, ConcurrentDictionary<string, int>> _invertedIndex;
+    private readonly string _pathToFolder;
+    private readonly string[] _separatingStrings;
+    
+    /// <param name="pathToFolder"><see cref="String"/> of path where stored files</param>
+    /// <param name="separatingStrings"> <see cref="Array.string"/> array of separating strings for document</param>
+    public InvertedIndex(string pathToFolder, string[] separatingStrings)
+    {
+        _invertedIndex = new ConcurrentDictionary<string, ConcurrentDictionary<string, int>>();
+        _pathToFolder = pathToFolder;
+        _separatingStrings = separatingStrings;
+    }
+
+    /// <summary>
+    /// Generates dictionary from files
+    /// </summary>
+    /// <param name="directories">List of directories where stores files</param>
+    /// <param name="threadCount">(should be > 0) How many threads will generate dictionary</param>
+    public void GenerateDictionary(List<string> directories, int threadCount = 8)
+    {
+        var allFiles = directories.Select(file => Directory.EnumerateFiles(_pathToFolder + file)).SelectMany(x => x).ToList();
+        var threads = new Thread[threadCount];
+        for (var i = 0; i < threadCount; i++)
+        {
+            var threadStart = allFiles.Count / threadCount * i;
+            var threadEnd = i == threadCount - 1 ? allFiles.Count : allFiles.Count / threadCount * (i + 1);
+
+            threads[i] = new Thread(() => BuildDictionary(allFiles, threadStart, threadEnd));
+            threads[i].Start();
+        }
+        for (var i = 0; i < threadCount; i++) { threads[i].Join(); }
+    }
+
+    /// <summary>
+    ///     Method building inverted index dictionary
+    /// </summary>
+    /// <param name="allFiles">
+    ///     <see cref="List{T}"/>
+    ///     list of all files that needed to be checked
+    /// </param>
+    /// <param name="Aind">first element in list</param>
+    /// <param name="Bind">last element in list</param>
+    private void BuildDictionary(List<string> allFiles, int Aind, int Bind)
+    {
+        for (var i = Aind; i < Bind; i++)
+        {
+            var file = allFiles[i];
+            var content = File.ReadAllText(file).ToLower().Split(_separatingStrings, StringSplitOptions.RemoveEmptyEntries).ToList();
+            addToIndex(content, file.Replace(_pathToFolder, ""));
+        }
+    }
+
+    private void addToIndex(List<string> words, string document)
+    {
+        foreach (var word in words)
+        {
+            if (!_invertedIndex.ContainsKey(word))
+            {
+                _invertedIndex.TryAdd(word, new ConcurrentDictionary<string, int>());
+                _invertedIndex[word].TryAdd(document, 1);
+            }
+            else if (!_invertedIndex[word].ContainsKey(document)) _invertedIndex[word].TryAdd(document, 1);
+            else _invertedIndex[word][document]++;
+        }
+    }
+
+    public void ClearDataBase() { _invertedIndex.Clear(); }
+    
+    /// <param name="text">text which needed to be found</param>
+    /// <returns>
+    ///     <see cref="ConcurrentDictionary{TKey,TValue}"/>
+    ///     where key is file, value is count of word in file.
+    /// </returns>
+    public ConcurrentDictionary<string, int> this[string text] => (_invertedIndex.TryGetValue(text, out var value) ? value : null)!;
+}
---
+++ InvertedIndexTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+var pathToFiles = @"..\..\..\..\files";
+
+var separatingStrings = new[] { ".", ",", "<br", " ", ":", ";", "/>", "<br/>", "\"", "?", "(", ")" , "{", "}", "@", "<", ">", "!"};
+var invertedIndex = new InvertedIndex(pathToFiles, separatingStrings);
+
+var directories = (from dir1 in Directory.EnumerateDirectories(pathToFiles)
+    from dir2 in Directory.EnumerateDirectories(dir1)
+    select dir2.Replace(pathToFiles, "")).ToList();
+
+
+var timer = new Stopwatch();
+const int maxThreads = 8;
+for (int i = 1; i <= maxThreads; i++)
+{
+    long averageTime = 0;
+    for (int j = 0; j < maxThreads; j++)
+    {
+        timer.Start();
+        invertedIndex.GenerateDictionary(directories, i);
+        timer.Stop();
+        averageTime += timer.ElapsedMilliseconds;
+        timer.Reset();
+        invertedIndex.ClearDataBase();
+    }
+
+    Console.WriteLine($"Using {i} threads we have average result {averageTime / maxThreads}ms");
+}
